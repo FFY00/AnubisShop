74e4713bfeb762eefe353a602a2a2d811f18e8fc
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-07 22:29:10 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/listeners/SignChangeListener.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * Simple Non Code License (SNCL) v1.10.0
+ */
+package cf.ffy00.shop.listeners;
+
+import org.bukkit.event.Listener;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public class SignChangeListener implements Listener{
+    
+}
