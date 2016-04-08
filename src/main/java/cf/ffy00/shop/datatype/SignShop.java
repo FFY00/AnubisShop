1736f40d32e01c5df9e58f8065b59e1dd663dc5d
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-08 20:53:04 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/datatype/SignShop.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * All Rights Reserved
+ */
+package cf.ffy00.shop.datatype;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public class SignShop {
+    
+}
