412fc76ad42e240b2ed1951099576884ad89ee96
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-08 21:10:42 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/listeners/LeavesDecayListener.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * Simple Non Code License (SNCL) v1.10.0
+ */
+package cf.ffy00.shop.listeners;
+
+import cf.ffy00.shop.ShopPlugin;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.event.block.LeavesDecayEvent;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public class LeavesDecayListener {
+    
+    public void onLeavesDecay(LeavesDecayEvent e){
+        Block b = e.getBlock();
+        if(b.getType().equals(Material.SIGN)
+                    || b.getType().equals(Material.SIGN_POST)
+                    || b.getType().equals(Material.WALL_SIGN)){
+            Sign s = (Sign) e.getBlock();
+            if(ShopPlugin.plugin.getApi().isShop(s)){
+                e.setCancelled(true);
+            }
+        }
+    }
+    
+}
