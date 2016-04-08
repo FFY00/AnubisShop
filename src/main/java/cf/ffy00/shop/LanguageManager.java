4db7cc84ab46783610e54998ee6a670e4977a81a
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-08 21:14:39 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/LanguageManager.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * Simple Non Code License (SNCL) v1.10.0
+ */
+package cf.ffy00.shop;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public class LanguageManager {
+    
+    public static String ERR_CANT_BREAK_SHOP = "§cYou can't break this shop!S";
+    
+}
