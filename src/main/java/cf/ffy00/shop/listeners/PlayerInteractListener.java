cdd43a269fcf5d599c1c0083b8d8ca2037e03ff5
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-08 20:28:44 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/listeners/PlayerInteractListener.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * Simple Non Code License (SNCL) v1.10.0
+ */
+package cf.ffy00.shop.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public final class PlayerInteractListener implements Listener{
+    
+    public void onPlayerInteract(PlayerInteractEvent e){
+        
+    }
+    
+}
