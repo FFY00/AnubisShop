fac058aac112fc514ba843a6afebf1ac40da1597
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-11 20:00:38 +0100

    Update

--- src/main/java/cf/ffy00/shop/listeners/PlayerInteractListener.java
+++ src/main/java/cf/ffy00/shop/listeners/PlayerInteractListener.java
@@ -8,6 +8,7 @@ package cf.ffy00.shop.listeners;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -18,6 +19,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
  */
 public final class PlayerInteractListener implements Listener{
     
+    @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e){
         if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.LEFT_CLICK_BLOCK)){
             Player p = (Player) e.getPlayer();
