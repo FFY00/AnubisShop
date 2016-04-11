5115f4b0b1d1eab93e6350e0a47c2fb750319d3e
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-11 22:15:02 +0100

    Update

--- src/main/java/cf/ffy00/shop/datatype/SignShop.java
+++ src/main/java/cf/ffy00/shop/datatype/SignShop.java
@@ -5,11 +5,27 @@
  */
 package cf.ffy00.shop.datatype;
 
+import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
+
 /**
  *
  * @author FFY00 <FFY00 at ffy00.cf>
  */
 public class SignShop {
+    
+    /*
+                             Sign Model
+    
+    0:         Player Name               <- Just for info
+    1:   B BUY_PRICE:SELL_PRICE S        <- Just for info
+    2:     ITEM NAME (MATERIAL)          <- Just for info
+    3:          SHOP #1                  <- SHOP ID! Where the magic happens...
+    */
+    
+    public SignShop(Sign s){
+        String id = ChatColor.stripColor(s.getLine(3)).split("#")[1];
+    }
     
     public String getOwner(){
         return "";
