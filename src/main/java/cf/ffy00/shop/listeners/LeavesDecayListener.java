bfb77b1db1a583b2936e7bfb3ed4db2c45e52d3e
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-11 20:00:58 +0100

    Update

--- src/main/java/cf/ffy00/shop/listeners/LeavesDecayListener.java
+++ src/main/java/cf/ffy00/shop/listeners/LeavesDecayListener.java
@@ -9,20 +9,23 @@ import static cf.ffy00.shop.ShopAPI.isShop;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
 
 /**
  *
  * @author FFY00 <FFY00 at ffy00.cf>
  */
-public class LeavesDecayListener {
+public class LeavesDecayListener implements Listener{
     
+    @EventHandler
     public void onLeavesDecay(LeavesDecayEvent e){
         Block b = e.getBlock();
         if(b.getType().equals(Material.SIGN)
                     || b.getType().equals(Material.SIGN_POST)
                     || b.getType().equals(Material.WALL_SIGN)){
-            Sign s = (Sign) e.getBlock();
+            Sign s = (Sign) b.getState();
             if(isShop(s)){
                 e.setCancelled(true);
             }
