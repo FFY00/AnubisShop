ef2f6efc640f4526521ba4ede8e1b2036c81a1dd
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-13 19:37:58 +0100

    Update

--- src/main/java/cf/ffy00/shop/ShopPlugin.java
+++ src/main/java/cf/ffy00/shop/ShopPlugin.java
@@ -6,7 +6,6 @@
 
 package cf.ffy00.shop;
 
-import static cf.ffy00.shop.ShopAPI.writeFile;
 import cf.ffy00.shop.listeners.BlockBreakListener;
 import cf.ffy00.shop.listeners.LeavesDecayListener;
 import cf.ffy00.shop.listeners.SignChangeListener;
@@ -14,10 +13,9 @@ import cf.ffy00.shop.listeners.PlayerInteractListener;
 import com.lenis0012.database.Database;
 import com.lenis0012.database.DatabaseConfigBuilder;
 import com.lenis0012.database.DatabaseFactory;
+import com.lenis0012.database.Table;
 import java.io.File;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -37,6 +35,10 @@ public final class ShopPlugin extends JavaPlugin {
     private static File dbFile;
     private static DatabaseFactory dbf;
     private static Database db;
+    
+    // Table Vars
+    private static Table TABLE_SHOPS;
+    private static Table TABLE_CHESTS;
 
     @Override
     public void onEnable() {
@@ -93,7 +95,63 @@ public final class ShopPlugin extends JavaPlugin {
     * Setup the Database Tables
     */
     private void setupDatabaseTables(){
-        
+        TABLE_SHOPS = new Table("shops", "id INT NOT NULL AUTO_INCREMENT, name VARCHAR(16), PRIMARY KEY(id)");
+        TABLE_CHESTS = new Table("chests", "id INT NOT NULL, "
+                + "slot1 VARCHAR(24), "
+                + "slot2 VARCHAR(24), "
+                + "slot3 VARCHAR(24), "
+                + "slot4 VARCHAR(24), "
+                + "slot5 VARCHAR(24), "
+                + "slot6 VARCHAR(24), "
+                + "slot7 VARCHAR(24), "
+                + "slot8 VARCHAR(24), "
+                + "slot9 VARCHAR(24), "
+                + "slot10 VARCHAR(24), "
+                + "slot11 VARCHAR(24), "
+                + "slot12 VARCHAR(24), "
+                + "slot13 VARCHAR(24), "
+                + "slot14 VARCHAR(24), "
+                + "slot15 VARCHAR(24), "
+                + "slot16 VARCHAR(24), "
+                + "slot17 VARCHAR(24), "
+                + "slot18 VARCHAR(24), "
+                + "slot19 VARCHAR(24), "
+                + "slot20 VARCHAR(24), "
+                + "slot21 VARCHAR(24), "
+                + "slot22 VARCHAR(24), "
+                + "slot23 VARCHAR(24), "
+                + "slot24 VARCHAR(24), "
+                + "slot25 VARCHAR(24), "
+                + "slot26 VARCHAR(24), "
+                + "slot27 VARCHAR(24), "
+                + "slot28 VARCHAR(24), "
+                + "slot29 VARCHAR(24), "
+                + "slot30 VARCHAR(24), "
+                + "slot31 VARCHAR(24), "
+                + "slot32 VARCHAR(24), "
+                + "slot33 VARCHAR(24), "
+                + "slot34 VARCHAR(24), "
+                + "slot35 VARCHAR(24), "
+                + "slot36 VARCHAR(24), "
+                + "slot37 VARCHAR(24), "
+                + "slot38 VARCHAR(24), "
+                + "slot39 VARCHAR(24), "
+                + "slot40 VARCHAR(24), "
+                + "slot41 VARCHAR(24), "
+                + "slot42 VARCHAR(24), "
+                + "slot43 VARCHAR(24), "
+                + "slot44 VARCHAR(24), "
+                + "slot45 VARCHAR(24), "
+                + "slot46 VARCHAR(24), "
+                + "slot47 VARCHAR(24), "
+                + "slot48 VARCHAR(24), "
+                + "slot49 VARCHAR(24), "
+                + "slot50 VARCHAR(24), "
+                + "slot51 VARCHAR(24), "
+                + "slot52 VARCHAR(24), "
+                + "slot53 VARCHAR(24), "
+                + "slot54 VARCHAR(24), "
+                + "PRIMARY KEY(id)");
     }
     
     /*
