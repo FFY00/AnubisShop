71dddd58fbca020a4cbd9c65cc204b45d559baf8
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-13 20:07:26 +0100

    Update

--- src/main/java/cf/ffy00/shop/CommandManager.java
+++ src/main/java/cf/ffy00/shop/CommandManager.java
@@ -5,10 +5,33 @@
  */
 package cf.ffy00.shop;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 /**
  *
  * @author FFY00 <FFY00 at ffy00.cf>
  */
-public class CommandManager {
+public class CommandManager implements CommandExecutor{
+    
+    private ShopPlugin plugin;
+    
+    public CommandManager(ShopPlugin plugin){
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] agrs) {
+        if(label.equalsIgnoreCase("shop")){
+            
+            return true;
+        } else if(label.equalsIgnoreCase("shopadmin")){
+            
+            return true;
+        } else {
+            return false;
+        }
+    }
     
 }
