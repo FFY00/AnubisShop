95e17d20df91e8fd7a9357c03193524dad2b3493
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-13 19:52:34 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/listeners/InventoryCloseListener.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * All Rights Reserved
+ */
+package cf.ffy00.shop.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public class InventoryCloseListener implements Listener{
+    
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e){
+        
+    }
+    
+}
