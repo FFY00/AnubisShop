8fd7ac5441dd4c3b6c92526a07cfbbcca3488273
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-13 19:53:59 +0100

    Update

---
+++ src/main/java/cf/ffy00/shop/listeners/InventoryClickListener.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2016 FFY00
+ * 
+ * All Rights Reserved
+ */
+package cf.ffy00.shop.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+/**
+ *
+ * @author FFY00 <FFY00 at ffy00.cf>
+ */
+public class InventoryClickListener implements Listener{
+    
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event){
+        
+    }
+    
+}
