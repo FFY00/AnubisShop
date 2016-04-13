d32652d2f151b1ce6b2ad1feaaacc49e1ee69bb5
Filipe Laíns FFY00 <filipe.lains@gmail.com>
2016-04-13 19:18:09 +0100

    Update

--- src/main/java/cf/ffy00/shop/ShopPlugin.java
+++ src/main/java/cf/ffy00/shop/ShopPlugin.java
@@ -11,8 +11,15 @@ import cf.ffy00.shop.listeners.BlockBreakListener;
 import cf.ffy00.shop.listeners.LeavesDecayListener;
 import cf.ffy00.shop.listeners.SignChangeListener;
 import cf.ffy00.shop.listeners.PlayerInteractListener;
+import com.lenis0012.database.Database;
+import com.lenis0012.database.DatabaseConfigBuilder;
+import com.lenis0012.database.DatabaseFactory;
 import java.io.File;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,24 +35,29 @@ public final class ShopPlugin extends JavaPlugin {
     private static File pluginsFolder;
     private static File pluginFolder;
     private static File dbFile;
+    private static DatabaseFactory dbf;
+    private static Database db;
 
     @Override
     public void onEnable() {
-        plugin = this;
         pl = getDescription();
         Bukkit.getConsoleSender().sendMessage("§bEnabling §cAnubisShop §bv" + pl.getVersion() + " by FFY00!");
         setupConfig();
+        createDatabase();
         
         // Declare Listenerss
         getServer().getPluginManager().registerEvents(new SignChangeListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
         getServer().getPluginManager().registerEvents(new LeavesDecayListener(), this);
+        
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
         Bukkit.getConsoleSender().sendMessage("§bDisabling §cAnubisShop §bv" + pl.getVersion() + " by FFY00!");
+        db.close();
     }
     
     /*
@@ -55,13 +67,32 @@ public final class ShopPlugin extends JavaPlugin {
         bukkitFolder = getDataFolder();
         pluginsFolder = new File(bukkitFolder, "plugins");
         pluginFolder = new File(pluginsFolder, pl.getName());
-        dbFile = new File(pluginFolder, "data");
         if(!new File (bukkitFolder, "config.yml").exists()){
             saveDefaultConfig();
         }
-        if(!dbFile.exists()){
-            writeFile(getResource("data.sqlite"), dbFile);
+    }
+    
+    /*
+    * Setup Database
+    */
+    private void createDatabase(){
+        dbf = new DatabaseFactory(plugin);
+        dbFile = new File(pluginFolder, "data.db");
+        ConfigurationSection section = getConfig().getConfigurationSection("MySQL");
+        DatabaseConfigBuilder config = new DatabaseConfigBuilder(section, dbFile);
+        db = dbf.getDatabase(config);
+        try {
+            db.connect();
+        } catch (SQLException ex) {
+            Bukkit.getConsoleSender().sendMessage("§bError conecting with the database! Disabling....");
+            Bukkit.getPluginManager().disablePlugin(this);
         }
     }
     
+    /*
+    * Return DatabaseFactory
+    */
+    public static Database getDb(){
+        return db;
+    }
 }
